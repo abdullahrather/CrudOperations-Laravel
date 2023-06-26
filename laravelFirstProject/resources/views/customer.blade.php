e810c1b3b3634fd366cf973e40130b1341c7c14f
Abdullah Rather <abdullahrather19@gmail.com>
2023-06-26 23:32:50 +0500

    Update customer.blade.php

--- laravelFirstProject/resources/views/customer.blade.php
+++ laravelFirstProject/resources/views/customer.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Title</title>
+    <title>Customer Registration</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,6 +10,20 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        .container {
+            background-color: #f7f7f7;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            max-width: 500px;
+            margin: 0 auto;
+        }
+
+        .form-group.row {
+            margin-bottom: 15px;
+        }
+    </style>
 </head>
 
 <body>
@@ -17,78 +31,87 @@
         @csrf
         <div class="container">
             <h1 class="text-center">Registration</h1>
-            <div class="form-group">
-                <label for="">Name</label>
-                <input type="text" name="name" id="" class="form-control" value="{{ old('name') }}">
-                <span class="danger">
-                    @error('name')
-                        {{ $message }}
-                    @enderror
-                </span>
-            </div>
-            <div class="form-group">
-                <label for="">Email</label>
-                <input type="email" name="email" id="" class="form-control" value="{{ old('email') }}">
-                <span class="danger">
-                    @error('email')
-                        {{ $message }}
-                    @enderror
-                </span>
-            </div>
-            <div class="form-group">
-                <label for="">Password</label>
-                <input type="password" name="password" id="" class="form-control">
-                <span class="danger">
-                    @error('password')
-                        {{ $message }}
-                    @enderror
-                </span>
-            </div>
-            <div class="form-group">
-                <label for="">Confirm Password</label>
-                <input type="password" name="password_confirmation" id="" class="form-control">
-                <span class="danger">
-                    @error('password_confirmation')
-                        {{ $message }}
-                    @enderror
-                </span>
-            </div>
-            <div class="form-group">
-                <label for="">Country</label>
-                <input type="text" name="country" id="" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="">State</label>
-                <input type="text" name="state" id="" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="">Address</label>
-                <textarea class="form-control" name="address" id="" rows="3"></textarea>
-            </div>
-            <div class="form-group">
-                <label for="gender">Gender:</label>
-                <div class="form-check">
-                    <label class="form-check-label">
-                        <input type="radio" class="form-check-input" name="gender" id="" value="M">
-                        Male
-                    </label>
+            <div class="form-group row">
+                <div class="col-md-6">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" class="form-control"
+                        value="{{ old('name') }}">
+                    <span class="text-danger">
+                        @error('name')
+                            {{ $message }}
+                        @enderror
+                    </span>
                 </div>
-                <div class="form-check">
-                    <label class="form-check-label">
-                        <input type="radio" class="form-check-input" name="gender" id="" value="F">
-                        Female
-                    </label>
+                <div class="col-md-6">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" class="form-control"
+                        value="{{ old('email') }}">
+                    <span class="text-danger">
+                        @error('email')
+                            {{ $message }}
+                        @enderror
+                    </span>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-md-6">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                    <span class="text-danger">
+                        @error('password')
+                            {{ $message }}
+                        @enderror
+                    </span>
+                </div>
+                <div class="col-md-6">
+                    <label for="password_confirmation">Confirm Password</label>
+                    <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
+                    <span class="text-danger">
+                        @error('password_confirmation')
+                            {{ $message }}
+                        @enderror
+                    </span>
                 </div>
             </div>
             <div class="form-group">
-                <label for="">Date of Birth</label>
-                <input type="date" name="DOB" class="form-control">
+                <label for="country">Country</label>
+                <input type="text" name="country" id="country" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="state">State</label>
+                <input type="text" name="state" id="state" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="address">Address</label>
+                <textarea class="form-control" name="address" id="address" rows="3"></textarea>
+            </div>
+            <div class="form-group row">
+                <div class="col-md-6">
+                    <label for="gender">Gender</label>
+                    <div class="form-check">
+                        <label class="form-check-label">
+                            <input type="radio" class="form-check-input" name="gender" id="male" value="M">
+                            Male
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <label class="form-check-label">
+                            <input type="radio" class="form-check-input" name="gender" id="female" value="F">
+                            Female
+                        </label>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <label for="dob">Date of Birth</label>
+                    <input type="date" name="DOB" id="DOB" class="form-control">
+                </div>
             </div>
             <button class="btn btn-primary">
                 Submit
             </button>
         </div>
     </form>
+
 </body>
 
 </html>
